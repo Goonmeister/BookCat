0139b891d22591663d5f5b80eb5fc3351c6a8f65
Gordon Dragoon <gordondragoon@MinasTirith.local>
2014-06-06 15:36:22 -0400

    Got the book-list and book-detail pages to work

--- app/js/app.js
+++ app/js/app.js
@@ -12,8 +12,12 @@ bookcatApp.config(["$routeProvider",
 	function($routeProvider) {
 		$routeProvider.
 			when('/books', {
-				templateUrl: 'partials/list.html',
+				templateUrl: 'partials/book-list.html',
 				controller: 'BookListCtrl'
+			}).
+			when('/books/:bookId', {
+				templateUrl: 'partials/book-detail.html',
+				controller: 'BookDetailCtrl'
 			}).
 			otherwise({
 				redirectTo: '/books'
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,3 +6,14 @@ bookcatControllers.controller('BookListCtrl', ['$scope', 'Book',
 	function($scope, Book) {
 		$scope.books = Book.query();
 	}]);
+
+bookcatControllers.controller('BookDetailCtrl', ['$scope', '$routeParams', 'Book',
+  function($scope, $routeParams, Book) {
+    $scope.book = Book.get({bookId: $routeParams.bookId}, function(book) {
+      $scope.mainImageUrl = "";
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
